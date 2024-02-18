0c17d22ac720286a136ced5eed113a5b9f5ca0a1
Michael Christianson <mikedc94@gmail.com>
2024-02-17 18:56:54 -0600

    corrected repo name

---
+++ docker_deploy.bash
@@ -0,0 +1,5 @@
+docker build . -t sous_chef_app
+# docker run -p 8080:8080 -t -i sous_chef_app:latest
+docker tag sous_chef_app:latest us-central1-docker.pkg.dev/sous-chef-chat-ai/sous-chef-app/sous_chef_app
+docker push us-central1-docker.pkg.dev/sous-chef-chat-ai/sous-chef-app/sous_chef_app
+gcloud run deploy sous-chef-app --image=us-central1-docker.pkg.dev/sous-chef-chat-ai/sous-chef-app/sous_chef_app:latest
